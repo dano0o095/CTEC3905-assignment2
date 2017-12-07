955172a8bafcbdcf47468c944fc97c1e61c1e190
dano0o095 <danokelly95@hotmail.com>
2017-12-07 17:46:45 +0000

    changed fonts and adjusted for mobile & desktop sizes. also commented out smooth scroll out in main.js

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ const topOfNav = nav.offsetTop;
 });
 
 })();
+
+
+
+/* Scroll to a certain element*/
+/*let education = document.getElementById('education');
+let marginY = 0;
+let destination = 0;
+let speed = 15;
+let scroller = null;
+
+education.addEventListener("click", initScroll(), false);
+
+function initScroll(elementId){
+  destination = document.getElementById(elementId).offsetTop;
+  
+  scroller = setTimeout(function(){
+    initScroll(elementId);
+  }, 1);
+  
+  marginY += speed;
+  
+  if(marginY >= destination){
+    clearTimeout(scroller);
+  }
+  
+  window.scroll(0, marginY);
+
+}*/
