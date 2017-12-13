7fec62a35126472990526b3c89e7e1ac17cac65e
dano0o095 <danokelly95@hotmail.com>
2017-12-13 10:20:38 +0000

    added a google maps api

--- main.js
+++ main.js
@@ -28,17 +28,41 @@ const topOfNav = nav.offsetTop + 400;
   let toggle = false; // hidden at first
   function toggleMenu(){
     if (toggle) { // true: it's visible
-      navMenu.style.backgroundColor = "#CAD178";
       navMenu.classList.remove("show-menu"), // hide it
       toggle = false
     }
     else { // false: it's hidden
+      navMenu.style.backgroundColor = "#CAD178";
       navMenu.classList.add("show-menu"), // show it
       toggle = true
     }
   }
 
 })();
+
+/*any click ouside the menu will close it*/
+/*let specifiedElement = document.querySelector('navBar');
+document.addEventListener('click', function(event) {
+  let isClickInside = specifiedElement.contains(event.target);
+  if (!isClickInside && nav.className == "show-menu") {
+    nav.className = "";
+  } 
+});*/
+
+
+
+/*GOOGLE MAPS API*/
+ function initMap() {
+    let uluru = {lat: 52.629934, lng: -1.139419};
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: uluru
+    });
+    let marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
 
 
 /* Scroll to a certain element*/
